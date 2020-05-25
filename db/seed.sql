a0f39f3fa3abb60f72a81300119b5a5b539fbb19
Alvaro Nieto <60838509+Anieto86@users.noreply.github.com>
2020-05-25 15:05:46 +0930

    server.js

--- db/seed.sql
+++ db/seed.sql
@@ -3,3 +3,5 @@
 INSERT INTO burgers (burger_name,devoured ) VALUES ('BACON CHEESS BURGER');
 INSERT INTO burgers (burger_name,devoured ) VALUES ('CHICKEN BURGER');
 INSERT INTO burgers (burger_name,devoured) VALUES ('ROYAL BURGER');
+INSERT INTO burgers (burger_name,devoured) VALUES ('CANGREBURGER');
+INSERT INTO burgers (burger_name,devoured) VALUES ('ROYAL CAGREBURGER');
--- sever.js
+++ sever.js
@@ -1,22 +1,27 @@
+//Dependencies
 let express = require('express');
-
-// Import routes and give the server access to them.
-var routes = require("./controllers/burger_controller.js");
-
 // Set Handlebars.
 var exphbs = require("express-handlebars");
-
-
-var PORT = process.env.PORT || 8000;
-
+//Express
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
 
+var PORT = process.env.PORT || 8000;
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set Handlebars.
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burger_controller.js");
 
 app.use(routes);
 
